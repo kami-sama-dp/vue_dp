35e91fc20dda36f0f3ca623b0adafdb9d09cbed3
pu.deng@mihoyo.com <pu.deng@mihoyo.com>
2021-01-12 20:27:25 +0800

    'admin'

--- vueadmin/src/main.js
+++ vueadmin/src/main.js
@@ -2,8 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css'
+import './styles/element-variables.scss'
+import 'normalize.css/normalize.css'  //css样式初始化
+import './icons'
+import './styles/index.scss';
 
 Vue.config.productionTip = false
+
+
+Vue.use(ElementUI)
 
 new Vue({
   router,
